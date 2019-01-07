894a69026d1297101fa78c4f3de9c60d438edccd
MarinJuricev <mjuric00@fesb.hr>
2019-01-07 21:42:57 +0100

    Displays error handling in the example

--- app/src/main/java/com/marinj/cleantutorial/core/Failure.kt
+++ app/src/main/java/com/marinj/cleantutorial/core/Failure.kt
@@ -3,4 +3,6 @@ package com.marinj.cleantutorial.core
 sealed class Failure{
     class NetworkConnection: Failure()
     class ServerError: Failure()
+
+    abstract class FeatureFail: Failure()
 }
--- app/src/main/java/com/marinj/cleantutorial/presentation/QuoteActivity.kt
+++ app/src/main/java/com/marinj/cleantutorial/presentation/QuoteActivity.kt
@@ -1,11 +1,12 @@
 package com.marinj.cleantutorial.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.marinj.cleantutorial.R
+import com.marinj.cleantutorial.core.Failure
 import kotlinx.android.synthetic.main.activity_main.*
 
 class QuoteActivity : AppCompatActivity() {
@@ -17,8 +18,16 @@ class QuoteActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         quoteViewModel = ViewModelProviders.of(this).get(QuoteViewModel::class.java)
-        quoteViewModel.randomQuote.observe(this, Observer{
+        quoteViewModel.randomQuote.observe(this, Observer {
             Toast.makeText(baseContext, "${it.quote} by ${it.author}", Toast.LENGTH_SHORT).show()
+        })
+
+        quoteViewModel.failure.observe(this, Observer {
+            when (it) {
+                is Failure.FeatureFail -> Toast.makeText(baseContext, "Feature Fail", Toast.LENGTH_SHORT).show()
+                is Failure.NetworkConnection -> Toast.makeText(baseContext, "No network connection", Toast.LENGTH_SHORT).show()
+                is Failure.ServerError -> Toast.makeText(baseContext, "AT sjebo", Toast.LENGTH_SHORT).show()
+            }
         })
 
         setupButtonListener()
