5e9565f16a4d4cc8455339e1e065a8794f76a28e
MarinJuricev <mjuric00@fesb.hr>
2019-01-08 20:58:10 +0100

    Adds a fake network call to ilustrate how the extension mapper works

--- app/src/main/java/com/marinj/cleantutorial/data/QuoteRepository.kt
+++ app/src/main/java/com/marinj/cleantutorial/data/QuoteRepository.kt
@@ -2,17 +2,30 @@ package com.marinj.cleantutorial.data
 
 import com.marinj.cleantutorial.core.Either
 import com.marinj.cleantutorial.core.Failure
+import com.marinj.cleantutorial.core.toQuote
+import com.marinj.cleantutorial.data.model.RemoteQuote
 import com.marinj.cleantutorial.domain.Quote
 import com.marinj.cleantutorial.domain.QuoteRepository
+import java.lang.Thread.sleep
 
 class Network : QuoteRepository {
     override fun getQuote(): Either<Failure, Quote> {
 
         val isNetworkAvailable = true
 
+        val quoteToReturn = getQuoteFromFakeNetwork()
+
         return when (isNetworkAvailable) {
-            true -> Either.Right(Quote("Nobody important", "Take nothing for granted"))
+            true -> Either.Right(quoteToReturn)
             false -> Either.Left(Failure.NetworkConnection())
         }
+    }
+
+    private fun getQuoteFromFakeNetwork(): Quote {
+        val networkQuote = RemoteQuote("important", "Take nothing for granted")
+
+        sleep(500)
+
+        return networkQuote.toQuote()
     }
 }
